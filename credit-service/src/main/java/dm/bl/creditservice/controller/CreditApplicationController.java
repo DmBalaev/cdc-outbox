2e7583d49102fc7eb8704555d7291ce508d13dbf
Akira <@>
2024-12-21 14:37:05 +0300

    TASK-1: Добвлены методы получения всех заявок и изменениия статуса

--- credit-service/src/main/java/dm/bl/creditservice/controller/CreditApplicationController.java
+++ credit-service/src/main/java/dm/bl/creditservice/controller/CreditApplicationController.java
@@ -2,24 +2,46 @@ package dm.bl.creditservice.controller;
 
 
 import dm.bl.api.CreditApplicationsApi;
+import dm.bl.creditservice.entity.CreditApplicationEntity;
+import dm.bl.creditservice.entity.CreditStatus;
 import dm.bl.creditservice.mapper.CreditApplicationMapper;
 import dm.bl.creditservice.service.CreditApplicationService;
 import dm.bl.model.CreditApplicationRequest;
 import dm.bl.model.CreditApplicationResponse;
-import lombok.RequiredArgsConstructor;
+import dm.bl.model.UpdateCreditApplicationStatusRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.UUID;
+
 @RestController
-@RequiredArgsConstructor
 @RequestMapping("/api/v1")
 public class CreditApplicationController implements CreditApplicationsApi {
     private final CreditApplicationService creditApplicationService;
     private final CreditApplicationMapper creditApplicationMapper;
 
+    public CreditApplicationController(CreditApplicationService creditApplicationService, CreditApplicationMapper creditApplicationMapper) {
+        this.creditApplicationService = creditApplicationService;
+        this.creditApplicationMapper = creditApplicationMapper;
+    }
+
     @Override
     public ResponseEntity<CreditApplicationResponse> createCreditApplication(CreditApplicationRequest creditApplicationRequest) {
         return ResponseEntity.ok(creditApplicationMapper.toCreditApplicationResponse(creditApplicationService.save(creditApplicationRequest)));
+    }
+
+    @Override
+    public ResponseEntity<List<CreditApplicationResponse>> getAllCreditApplications() {
+        return ResponseEntity.ok(creditApplicationService.getAllApplication().stream()
+                .map(creditApplicationMapper::toCreditApplicationResponse)
+                .toList());
+    }
+
+    @Override
+    public ResponseEntity<CreditApplicationResponse> updateCreditApplicationStatus(UUID id, UpdateCreditApplicationStatusRequest updateCreditApplicationStatusRequest) {
+        CreditApplicationEntity application = creditApplicationService.updateStatus(id, CreditStatus.valueOf(updateCreditApplicationStatusRequest.getStatus().toUpperCase()));
+        return ResponseEntity.ok(creditApplicationMapper.toCreditApplicationResponse(application));
     }
 }
--- credit-service/src/main/java/dm/bl/creditservice/service/CreditApplicationService.java
+++ credit-service/src/main/java/dm/bl/creditservice/service/CreditApplicationService.java
@@ -10,6 +10,8 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -26,4 +28,19 @@ public class CreditApplicationService {
                 .build();
         return creditApplicationRepository.save(newCreditApplication);
     }
+
+    @Transactional(readOnly = true)
+    public List<CreditApplicationEntity> getAllApplication() {
+        return creditApplicationRepository.findAll();
+    }
+
+    @Transactional
+    public CreditApplicationEntity updateStatus(UUID applicationid, CreditStatus status) {
+        CreditApplicationEntity application = creditApplicationRepository.findById(applicationid)
+                .orElseThrow(() -> new RuntimeException("Application not found"));
+        application.setStatus(status);
+        application.setUpdateAt(LocalDate.now());
+        return creditApplicationRepository.save(application);
+    }
 }
+
